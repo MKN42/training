0d21119318307e795896bb94322ffc7d34ace50b
Matthias Knappik <matthias.knappik@posteo.de>
2026-03-17 14:46:05 +0100

    Initial commit

---
+++ src/test/java/de/mkn/training/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mkn.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
